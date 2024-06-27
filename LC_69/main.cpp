1fbc1cbe17554fcbee0f4babe8f1f505a2098068
sanket9673 <sanketch9673@gmail.com>
2024-06-27 23:40:28 +0530

    Code File

---
+++ LC_69/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        // Calculating the square root with binary search {O(log n)}
+        int l = 0;
+        int r = x;
+        int res = 0;
+
+        while (l <= r) {
+            long long m = l + (r - l) / 2;
+            if (m * m < x) {
+                res = m;
+                l = m + 1;
+            }
+            else if (m * m > x) {
+                r = m - 1;
+            }
+            else {
+                return m;
+            }
+        }
+        return res;
+    }
+};
