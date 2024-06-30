0a431e29de75cd96a811fabd93681fb363e02be2
sanket9673 <sanketch9673@gmail.com>
2024-06-30 23:26:39 +0530

    Valid Perfect Square

--- LC_367/main.cpp
+++ LC_367/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include<map>
+#include<string>
+
 using namespace std ;
 
 class Solution {
@@ -8,19 +11,23 @@ public:
         int l = 0;
         int r = num;
         int res = false;
+        
         while (l <= r) {
             long long mid = l + (r - l) / 2;
             if (mid * mid < num) {
                 res = false;
                 l = mid + 1 ;
             }
+
             else if (mid * mid > num) {
                 r = mid - 1;
             }
+
             else {
                 return true;
             }
         }
+
         return res ;
     }
 };
