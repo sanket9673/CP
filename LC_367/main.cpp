45136729345a95b9977e5888f07c0d5e7c0d7cdd
sanket9673 <sanketch9673@gmail.com>
2024-06-30 23:24:08 +0530

    Valid Perfect Square

---
+++ LC_367/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int l = 0;
+        int r = num;
+        int res = false;
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            if (mid * mid < num) {
+                res = false;
+                l = mid + 1 ;
+            }
+            else if (mid * mid > num) {
+                r = mid - 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return res ;
+    }
+};
