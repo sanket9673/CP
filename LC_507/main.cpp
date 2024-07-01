281798e45959ce21eaf50ec8968568771e0d5234
sanket9673 <sanketch9673@gmail.com>
2024-07-01 18:51:59 +0530

    Perfect Number

---
+++ LC_507/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num){
+        int sum = 0 ;
+        if (num == 1) {
+            return false ;
+        }
+        else {
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    sum += i;
+                    if (i != num / i && i != 1) {
+                        sum += num / i ;
+                    }
+                }
+            }
+            if (sum == num) {
+                return true ;
+            }
+            else {
+                return false ;
+            }
+        }
+    }
+};
