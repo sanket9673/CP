6cabd08f08a63b30cde68b0dc56a0d89c5c56d1b
sanket9673 <sanketch9673@gmail.com>
2024-07-02 23:49:10 +0530

    Code Files

--- LC_412/main.cpp
+++ LC_412/main.cpp
@@ -12,12 +12,15 @@ public:
             if (i % 3 == 0 && i % 5 == 0) {
                 res.push_back("FizzBuzz");
             } 
+
             else if (i % 3 == 0) {
                 res.push_back("Fizz");
             } 
+
             else if (i % 5 == 0) {
                 res.push_back("Buzz");
             } 
+            
             else {
                 res.push_back(to_string(i));
             }
