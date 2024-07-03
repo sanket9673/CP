021c45007b7c6811fbef04afab25939ebfb1917d
sanket9673 <sanketch9673@gmail.com>
2024-07-03 22:59:50 +0530

    Power of two

---
+++ LC_231/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false ;
+        }
+        else if (n < 0) {
+            return false ;
+        }
+        else if (static_cast<int>(log2(n)) == log2(n)) {
+            return true ;
+        }
+        else {
+            return false ;
+        }
+    }
+};
