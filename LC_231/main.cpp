8ecace18842d81b3da73a95c313deffa9b093035
sanket9673 <sanketch9673@gmail.com>
2024-07-03 23:03:02 +0530

    Power of two

--- LC_231/main.cpp
+++ LC_231/main.cpp
@@ -7,6 +7,7 @@ using namespace std ;
 
 class Solution {
 public:
+
     bool isPowerOfTwo(int n) {
         if (n == 0) {
             return false ;
@@ -19,7 +20,7 @@ public:
         else if (static_cast<int>(log2(n)) == log2(n)) {
             return true ;
         }
-        
+
         else {
             return false ;
         }
