62a7d6b659222883f13da3a9ac8943cb014ce56c
sanket9673 <sanketch9673@gmail.com>
2024-07-05 00:01:53 +0530

    First Palindromic string in array

---
+++ LC_2108/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        string res ;
+        for (auto word : words) {
+            res = word ;
+            reverse(res.begin(), res.end()) ;
+            if (word == res) {
+                return res ;
+            }
+        }
+        return "";
+    }
+};
