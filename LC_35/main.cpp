a6809a8ab0e36bb1c1c029883d64a94d643d1a41
sanket9673 <sanketch9673@gmail.com>
2024-07-11 00:23:18 +0530

    Search Insert Position

--- LC_35/main.cpp
+++ LC_35/main.cpp
@@ -6,15 +6,18 @@ using namespace std ;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+
         int res = 0 ;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == target) {
                 res = i;
                 break;
             }
+
             else if (nums[i] < target && i == nums.size() - 1) {
                 res = nums.size() ;
             }
+            
             else if (nums[i] > target) {
                 res = i ;
                 break ;
