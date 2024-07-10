4bfd77a55275593ea1abdf7b58587276c45820a5
sanket9673 <sanketch9673@gmail.com>
2024-07-11 01:32:37 +0530

    First and last position of element in sorted array

---
+++ LC_35/LC_34/main.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> nt = {-1,-1} ;
+        int n = nums.size() ;
+        int f = -1;
+        int s = 0 ;
+
+        if (nums.empty()) {
+            return nt ;
+        }
+
+        else {
+            for (int i = 0; i < n; i++) {
+                if (nums[i] == target && f == -1) {
+                    f = i ;
+                    s = f ;
+                }
+                else if (nums[i] == target) {
+                    s = i ;
+                }
+            }
+            if (f == -1) {
+                return nt ;
+            }
+            else {
+                return {f,s} ;
+            }
+        }
+
+    }
+};
