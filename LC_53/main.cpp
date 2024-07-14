6fa25dd17585d890c0be828f6760e45cd4d6d1f5
sanket9673 <sanketch9673@gmail.com>
2024-07-15 01:12:35 +0530

    Maximum Subarray

---
+++ LC_53/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0] ;
+        int maxEnd = nums[0] ;
+
+        for (int i = 1; i < nums.size(); i++) {
+            maxEnd = max(nums[i], maxEnd + nums[i]) ;
+            res = max (maxEnd, res) ;
+        }
+
+        return res ;
+    }
+};
