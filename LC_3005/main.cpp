d881c6c98a0944dc0121f6457657e97c250f5369
sanket9673 <sanketch9673@gmail.com>
2024-07-17 19:49:41 +0530

    Count Elements with max frequency

--- LC_3005/main.cpp
+++ LC_3005/main.cpp
@@ -9,10 +9,12 @@ public:
         sort(nums.begin(), nums.end()) ;
     int count = 1;
     vector<int> arr ;
+
     for (int i = 0; i < nums.size() - 1; i++){
         if (nums[i] == nums[i+1]) {
             count++ ;
         }
+
         else {
             arr.push_back(count) ;
             count = 1 ;
@@ -21,15 +23,18 @@ public:
     
     arr.push_back(count) ;
     int a = arr[0] ;
+
     for (int i = 1; i < arr.size(); i++) {
         a  = max(a, arr[i]) ;
     }
     int res = 0 ;
+
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] == a) {
             res += a ;
         }
     }
+    
     return res ;
 
     }
