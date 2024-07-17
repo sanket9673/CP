90fbb4169a2cf64a9fb58891a0afcbae5a2527a2
sanket9673 <sanketch9673@gmail.com>
2024-07-18 01:36:15 +0530

    Find the Pivot Integer

---
+++ LC_2485/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int arr[n] ;
+        int pref[n] ;
+        int sum = 0 ;
+        int res ;
+        for (int i = 0 ; i < n; i++) {
+            arr[i] = i + 1 ;
+            sum += arr[i] ;
+        }
+        pref[0] = arr[0] ;
+
+        for (int i = 1; i < n; i++) {
+            pref[i] = arr[i] + pref[i-1] ;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (pref[i] == sum - pref[i] + arr[i]) {
+                return arr[i] ;
+            }
+        }
+        return -1 ;
+    }
+};
