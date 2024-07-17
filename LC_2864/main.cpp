8e2b08eddfe3e525daab41f63fcfe3bcb62ccd48
sanket9673 <sanketch9673@gmail.com>
2024-07-18 00:06:15 +0530

    Max Odd Binary Number

---
+++ LC_2864/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int count = 0 ;
+        for (char c : s)
+            count += (c == '1') ;
+
+        for (char &c : s) 
+            c = '0' ;
+
+        if (count == 1) {
+            s[s.length() - 1] = '1' ;
+            return s ;
+        }
+        else {
+            for (int i = 0; i < count - 1; i++) {
+                s[i] = '1' ;
+            }
+            s[s.length() - 1] = '1' ;
+            return s ;
+        }
+        return "" ;
+    }
+};
