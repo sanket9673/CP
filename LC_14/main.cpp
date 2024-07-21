6f786373b559d15b0ee2b03edc3b368a1c663651
sanket9673 <sanketch9673@gmail.com>
2024-07-22 00:24:44 +0530

    Longest Common Prefix

---
+++ LC_14/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        // Arrange the string lexicographically from smallest to largest
+        string res = "" ;
+        if (strs.empty()) {
+            return res ;
+        }
+        else {
+            sort(strs.begin(), strs.end()) ;
+            string frt = strs[0] ;
+            string lst = strs[strs.size() - 1] ;
+            int a = min(frt.size(), lst.size()) ;
+
+            for (int i = 0; i < a; i++) {
+                if (frt[i] != lst[i]) {
+                    return res ;
+                }
+                res += frt[i] ;
+            }
+            return res ;
+        }
+        return "" ;
+    }
+};
