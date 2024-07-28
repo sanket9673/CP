01b0d54e62c8ea2a2f55bbccbf1f5bfe6cdc8b84
sanket9673 <sanketch9673@gmail.com>
2024-07-28 17:20:52 +0530

    Search in Rotated Array

---
+++ LC_33/main.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size() ;
+        if (n == 1) {
+            if (nums[0] == target) {
+                return 0 ;
+            }
+            return -1 ;
+        }
+        int l = 0 ;
+        int m = n - 1 ;
+        while (l <= m) {
+            int mid = l + (m - l) / 2 ;
+            if (target == nums[mid]) {
+                return mid ;
+            }
+
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    m = mid - 1 ;
+                }
+                else {
+                    l = mid + 1 ;
+                }
+            }
+
+            else {
+                if (nums[mid] < target && target <= nums[m]) {
+                    l = mid + 1 ;
+                }
+                else {
+                    m = mid - 1;
+                }
+            }
+        }
+        return -1 ;
+    }
+};
