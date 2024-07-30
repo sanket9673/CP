7360d22029230b0ba3703be952fa8943262d8f30
sanket9673 <sanketch9673@gmail.com>
2024-07-30 23:29:50 +0530

    Contains Duplicate

---
+++ LC_217/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int count = 1 ;
+
+        if (nums.size() == 1) {
+            return false ;
+        }
+        
+        else {
+            sort(nums.begin(), nums.end()) ;
+            for (int i = 1 ; i < nums.size(); i++) {
+                if (nums[i] == nums[i-1]) {
+                    count++ ;
+                    if (count == 2) {
+                        return true ;
+                    }
+                }
+                else {
+                    count = 1 ;
+                }
+            }
+            return false ;
+        }
+
+    }
+};
