f4ff89430800bf2596f832f364380de96298f881
sanket9673 <sanketch9673@gmail.com>
2024-08-01 23:22:31 +0530

    IsAnagram

---
+++ LC_242/main.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false ;
+        }
+        int a[256] = {0} ;
+        int b[256] = {0} ;
+
+        for (int i = 0; i < s.length(); i++) {
+            a[(s[i] - 'a')]++ ;
+            b[(t[i] - 'a')]++ ;
+        }
+
+        for (int i = 0; i <256 ;i++) {
+            if (a[i] != b[i]) {
+                return false ;
+            }
+        }
+        return true ;
+
+        // if (s.length() != t.length()) {
+        //     return false ;
+        // }
+
+        // sort(s.begin(), s.end()) ;
+        // sort(t.begin(), t.end()) ;
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s[i] != t[i]) {
+        //         return false ;
+        //     }
+        // }
+        // return true ;
+    }
+};
