f8b8fb5bba4c4a785143d6e21c7782d884ddff89
sanket9673 <sanketch9673@gmail.com>
2024-08-01 23:07:17 +0530

    countSenior

--- LC_2678/main.cpp
+++ LC_2678/main.cpp
@@ -6,6 +6,7 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int count = 0 ;
+        
         for (int i = 0; i < details.size(); i++) {
             string s = "" ;
             s += details[i][11] ;
