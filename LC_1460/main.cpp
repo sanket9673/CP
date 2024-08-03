4128f664c9b3951e542eeaf32cd1cd38805a1f6a
sanket9673 <sanketch9673@gmail.com>
2024-08-03 19:35:28 +0530

    Make two arrays equal by reversing subarrays

---
+++ LC_1460/main.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        if (target.size() != arr.size()) {
+            return false ;
+        }
+        
+        sort (target.begin(), target.end());
+        sort (arr.begin(), arr.end()) ;
+
+        for (int i = 0; i < target.size(); i++) {
+            if (target[i] != arr[i]) {
+                return false ;
+            }
+        }
+        return true ;
+        
+        // std::unordered_map<int, int> countMap;
+
+        // // Count occurrences in target
+        // for (int num : target) {
+        //     countMap[num]++;
+        // }
+
+        // // Subtract occurrences found in arr
+        // for (int num : arr) {
+        //     countMap[num]--;
+        // }
+
+        // // Check if all counts are zero
+        // for (const auto& pair : countMap) {
+        //     if (pair.second != 0) {
+        //         return false;
+        //     }
+        // }
+        // return true ;
+    }
+};
