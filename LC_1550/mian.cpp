f8be1a0df61eea150210f2ad5a9535718902b0ee
sanket9673 <sanketch9673@gmail.com>
2024-08-06 22:45:19 +0530

    Three Consecutive Odd

--- LC_1550/mian.cpp
+++ LC_1550/mian.cpp
@@ -6,8 +6,9 @@ class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int count = 0 ;
-        
+
         for (int i = 0; i < arr.size(); i++) {
+            
             if (arr[i] % 2 != 0) {
                 count++ ;
                 if (count == 3) {
