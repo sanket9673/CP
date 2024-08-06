51812964e2670db0489bac0390349953f50b9444
sanket9673 <sanketch9673@gmail.com>
2024-08-06 22:45:31 +0530

    Three Consecutive Odd

--- LC_1550/mian.cpp
+++ LC_1550/mian.cpp
@@ -8,7 +8,7 @@ public:
         int count = 0 ;
 
         for (int i = 0; i < arr.size(); i++) {
-            
+
             if (arr[i] % 2 != 0) {
                 count++ ;
                 if (count == 3) {
@@ -19,6 +19,7 @@ public:
                 count = 0 ;
             }
         }
+        
         return false ;
     }
 };
