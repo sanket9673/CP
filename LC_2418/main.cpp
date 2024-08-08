d5788414f90955d32afec9a0fe67d0676376e71d
sanket9673 <sanketch9673@gmail.com>
2024-08-08 19:24:35 +0530

    Sort People

---
+++ LC_2418/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int count = heights.size() ;
+        while (count > 0) {
+            for (int i = 0; i < heights.size() - 1; i++) {
+                if (heights[i] < heights[i + 1]) {
+                    swap(heights[i], heights[i+1]) ;
+                    swap(names[i], names[i+1]) ;
+                }
+            }
+            count-- ;
+        }
+        return names ;    
+    }
+};
