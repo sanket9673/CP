3e10f138485dde0c0e71e2178ffea188d3862aaa
sanket9673 <sanketch9673@gmail.com>
2024-08-10 15:21:28 +0530

    Find the Difference

---
+++ LC_389/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int arr[256] = {0} ;
+        for (int i = 0; i < s.length(); i++) {
+            arr[s[i]]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            arr[t[i]]-- ;
+        }
+        char res;
+        for (int i = 0; i < 256; i++) {
+            if (arr[i] < 0) {
+                res = char(i) ;
+            }
+        }
+        return res ;
+    }
+};
