9356944f5dabd446e1d371c8b8dc0b772add23c3
sanket9673 <sanketch9673@gmail.com>
2024-08-10 15:23:39 +0530

    Find the Difference

--- LC_389/main.cpp
+++ LC_389/main.cpp
@@ -6,11 +6,10 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int arr[256] = {0} ;
-        
+
         for (int i = 0; i < s.length(); i++) {
             arr[s[i]]++;
         }
-
         for (int i = 0; i < t.length(); i++) {
             arr[t[i]]-- ;
         }
