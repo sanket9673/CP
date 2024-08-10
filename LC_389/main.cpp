ddb1c6fd08d64854003b1775ef5a42064048a7eb
sanket9673 <sanketch9673@gmail.com>
2024-08-10 15:22:58 +0530

    Find the Difference

--- LC_389/main.cpp
+++ LC_389/main.cpp
@@ -10,10 +10,11 @@ public:
         for (int i = 0; i < s.length(); i++) {
             arr[s[i]]++;
         }
-        
+
         for (int i = 0; i < t.length(); i++) {
             arr[t[i]]-- ;
         }
+        
         char res;
         for (int i = 0; i < 256; i++) {
             if (arr[i] < 0) {
