6da650d682f44e072af95cf8c6058bbf2483bfa7
sanket9673 <sanketch9673@gmail.com>
2024-08-10 15:22:34 +0530

    Find the Difference

--- LC_389/main.cpp
+++ LC_389/main.cpp
@@ -5,6 +5,7 @@ using namespace std ;
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        
         int arr[256] = {0} ;
         for (int i = 0; i < s.length(); i++) {
             arr[s[i]]++;
