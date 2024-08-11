c94a1c30063c60716e33f4ae460c8777ae025cae
sanket9673 <sanketch9673@gmail.com>
2024-08-11 15:23:14 +0530

    Longest Palindrome Substring

---
+++ LC_5/main.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res = "" ;
+        int maxlength = 0 ;
+
+        if (s.length() == 1){
+            return s ;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            // for odd length palindrome
+            int left, right ;
+            left = i ;
+            right = i ;
+            while (left >= 0 && right <= s.length() && s[left] == s[right]) {
+                if (right - left + 1 > maxlength) {
+                    maxlength = right - left + 1 ;
+                    res = s.substr(left, maxlength) ;
+                }
+                left-- ;
+                right++ ;
+            }
+
+            // for even length palindrome
+            left = i ;
+            right = i+1 ;
+            while (left >= 0 && right <= s.length() && s[left] == s[right]) {
+                if (right - left + 1 > maxlength) {
+                    maxlength = right - left + 1 ;
+                    res = s.substr(left, maxlength) ;
+                }
+                left-- ;
+                right++ ;
+            }
+        }
+        return res ;
+    }
+};
