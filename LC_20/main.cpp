b800738f4b01910cf53e54c6f8a1461843375181
sanket9673 <sanketch9673@gmail.com>
2024-08-12 10:53:25 +0530

    Valid Paranthesis

---
+++ LC_20/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> stk ;
+        for (int i = 0; i < s.length(); i++) {
+            if (stk.empty()) {
+                stk.push(s[i]) ;
+            }
+            else if (stk.top() == '(' && s[i] == ')' || stk.top() == '[' && s[i] == ']' || stk.top() == '{' && s[i] == '}') {
+                stk.pop() ;
+            }
+            else {
+                stk.push(s[i]) ;
+            }
+        }
+
+        if (stk.empty()) {
+            return true ;
+        }
+        else {
+            return false ;
+        }
+    }
+};
