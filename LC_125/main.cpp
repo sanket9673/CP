f90101df8e510b512d9f86f7ddee37562bacc05c
sanket9673 <sanketch9673@gmail.com>
2024-08-12 22:05:25 +0530

    ValidPalindrome

---
+++ LC_125/main.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.empty()) {
+            return true ;
+        }
+        
+        string str = "" ;
+        string res ;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                str += s[i] ;
+            }
+            else if (s[i] >= 'A' && s[i] <= 'Z') {
+                str += tolower(s[i]) ;
+            }
+            else if (s[i] >= '0' && s[i] <= '9') {
+                str += s[i];
+            }
+        }
+        res = str ;
+        reverse(res.begin(), res.end()) ;
+        if (str == res) {
+            return true ;
+        }
+        else {
+            return false ;
+        }
+    }
+};
