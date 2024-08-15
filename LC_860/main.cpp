5353d1ef014ba540ffab67e38e6aff9f63caf6d6
sanket9673 <sanketch9673@gmail.com>
2024-08-15 22:23:23 +0530

    Lemonade Challenge

---
+++ LC_860/main.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        if (bills[0] > 5) {
+            return false ;
+        }
+        int count5 = 0 ;
+        int count10 = 0;
+
+        for (int i = 0; i < bills.size(); i++) {
+            if (bills[i] == 5) {
+                count5++ ;
+            }
+            else if (bills[i] == 10) {
+                if (count5 == 0) {
+                    return false ;
+                }
+                count10++ ;
+                count5 -- ;
+            }
+            else if (bills[i] == 20) {
+                if (count10 > 0 && count5 > 0) {
+                    count10--;
+                    count5--;
+                } else if (count5 >= 3) {
+                    count5 -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true ;
+    }
+};
