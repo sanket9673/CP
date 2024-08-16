ca272d2078b60d52cb81a8741012359b93025b70
sanket9673 <sanketch9673@gmail.com>
2024-08-17 00:15:00 +0530

    maxDistance

---
+++ LC_624/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int dist = 0 ;
+        int mn = arrays[0][0] ;
+        int mx = arrays[0].back() ; // To get the last element --> x arr[0][-1]
+
+        for (int i = 1; i < arrays.size(); i++) {
+            dist = max(dist, abs(mx - arrays[i][0])) ;
+            dist = max(dist, abs(mn - arrays[i].back())) ;
+            mn = min(mn, arrays[i][0]) ;
+            mx = max(mx, arrays[i].back()) ;
+        }
+        return dist ;
+    }
+};
