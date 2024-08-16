b92cf5667f0a3a06393067125a427e93b34a84c0
sanket9673 <sanketch9673@gmail.com>
2024-08-17 00:34:07 +0530

    Student Attendace Record I

---
+++ LC_551/main.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int countA = 0 ;
+        int countL = 0 ;
+        for (char c : s) {
+            if (c == 'A') {
+                countA++ ;
+            }
+        }
+        if (countA < 2) {
+            int res = INT_MIN ;
+            for (char c : s) {
+                if (c == 'L') {
+                    countL++ ;
+                    res = max(res, countL) ;
+                }
+                else {
+                    countL = 0 ;
+                }
+            }
+            
+            if (res > 2) {
+                return false ;
+            }
+            else {
+                return true ;
+            }
+        }
+        else {
+            return false ;
+        }
+    }
+};
