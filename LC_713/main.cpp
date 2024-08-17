506bb04adbf15e68e988fdbbedc6e036f25bedb1
sanket9673 <sanketch9673@gmail.com>
2024-08-18 04:53:25 +0530

    Subarray Product less than K

---
+++ LC_713/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int count = 0;
+        long long res = 0 ;
+        for (int i = 0; i < nums.size(); i++) {
+            res = nums[i] ;
+            if (nums[i] < k) {
+                count++ ;
+            }
+            for (int j = i+1; j < nums.size(); j++) {
+                if (res*nums[i] <= INT_MAX) { // So as answer doesn't cross the integer limit
+                    res *= nums[j] ;
+                    if (res < k) {
+                        count++ ;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return count ;
+    }
+};
