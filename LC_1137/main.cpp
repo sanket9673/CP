af7836cefd7d15b88cf05e1cb8110c1037b17516
sanket9673 <sanketch9673@gmail.com>
2024-08-18 05:15:54 +0530

    Nth Tribonacci Series

---
+++ LC_1137/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) {
+            return 0 ;
+        }
+        else if (n <= 2) {
+            return 1 ;
+        }
+        int a = 0 ;
+        int b = 1 ;
+        int c = a + b ;
+        int res ; 
+        for (int i = 3; i <= n; i++) {
+            res = a + b + c ;
+            a = b ;
+            b = c ;
+            c = res ;
+        }
+        return res ;
+    }
+};
