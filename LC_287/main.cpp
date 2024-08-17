7a6e71aa7cfe32630065f28ceb75d75f2c0db148
sanket9673 <sanketch9673@gmail.com>
2024-08-18 04:30:27 +0530

    Find the Duplicate Number

---
+++ LC_287/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size() ;
+        int res ;
+        vector<int> arr(n+1, 0);
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]]++ ;
+            if (arr[nums[i]] > 1) {
+                return nums[i] ;
+            }
+        }
+        return 0 ; 
+    }
+};
