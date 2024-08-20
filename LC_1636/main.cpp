a02ec1190ce35e4242a8dbd3e45607d9f873e3c8
sanket9673 <sanketch9673@gmail.com>
2024-08-20 19:22:01 +0530

    Sort array by increasing frequency

---
+++ LC_1636/main.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums ;
+        }
+        vector<int> res ;
+        sort(nums.begin(), nums.end()) ;
+        int count = 1 ;
+        vector<pair<int,int>>arr ;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i-1]) {
+                count++ ;
+            }
+            else {
+                arr.push_back({nums[i-1], count}) ;
+                count = 1 ;
+            }
+        }
+        
+        arr.push_back({nums.back(), count}) ;
+
+        sort(arr.begin(), arr.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            if (a.second == b.second) {
+                return a.first > b.first ;
+            }
+            return a.second < b.second;
+        });
+
+        for (const auto& pair : arr) {
+            for (int i = 0; i < pair.second; ++i) {
+                res.push_back(pair.first) ;
+            }
+        }
+
+        return res ;
+    }
+};
