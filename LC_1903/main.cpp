a4275f5c20b02504a0e0bbbd359d473a8e810652
sanket9673 <sanketch9673@gmail.com>
2024-08-21 11:43:23 +0530

    Largest Odd Number

---
+++ LC_1903/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        if ((num[num.length() - 1] - '0') % 2 != 0) {
+            return num ;
+        }
+        string res = "" ;
+        bool found = false ;
+        int count = 0 ;
+        for (int i = 0; i < num.length(); i++) {
+            if ((num[i] - '0') % 2 != 0) {
+                count = i ;
+                found = true ;
+            }
+        }
+        for (int i = 0 ; i <= count; i++) {
+            res += num[i] ;
+        }
+        if (found == false) {
+            return "" ;
+        }
+        return res ;
+    }
+};
