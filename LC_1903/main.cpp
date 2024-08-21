58bc7f906952acb04c5b29c22dadedc3699f936b
sanket9673 <sanketch9673@gmail.com>
2024-08-21 11:43:55 +0530

    Largest Odd Number

--- LC_1903/main.cpp
+++ LC_1903/main.cpp
@@ -7,6 +7,7 @@ public:
         if ((num[num.length() - 1] - '0') % 2 != 0) {
             return num ;
         }
+        
         string res = "" ;
         bool found = false ;
         int count = 0 ;
