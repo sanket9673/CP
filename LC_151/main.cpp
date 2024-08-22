4ca3253eb44e7cc484349afff70a80d7143ce6d6
sanket9673 <sanketch9673@gmail.com>
2024-08-22 18:30:04 +0530

    Reverse Word in String

---
+++ LC_151/main.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> arr ;
+        string res = "" ;
+        string demo = "" ;
+        for (int i = 0; i < s.length(); i++) {
+            if (isspace(s[i])) {
+                if (demo != "") {
+                    arr.push(demo) ;
+                    demo = "" ;
+                }
+            }
+            
+            if (!isspace(s[i])) {
+                demo += s[i] ;
+            }
+        }
+        if (demo != "") {
+            arr.push(demo) ;
+        }
+
+        while (!arr.empty()) {
+            res += arr.top();
+            arr.pop() ;
+            if (!arr.empty()) {
+                res += " " ;
+            }
+        }
+        return res ;
+    }
+};
