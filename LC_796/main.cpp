f20b744a69a42467f836d18208ca34507c5fec6f
sanket9673 <sanketch9673@gmail.com>
2024-08-26 21:58:03 +0530

    Rotate String

--- LC_796/main.cpp
+++ LC_796/main.cpp
@@ -7,12 +7,13 @@ public:
         if (s.length() == 1) {
             return true ;
         }
-        
+
         if (s.length() != goal.length()) {
             return false ;
         }
 
         int count = 0 ;
+        
         while (count < s.length()) {
             s = s.substr(1) + s[0] ;
             if (s == goal) {
