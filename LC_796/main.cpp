0a137b40ee823905f90f0e50b334735423ea80e3
sanket9673 <sanketch9673@gmail.com>
2024-08-26 21:57:05 +0530

    Rotate String

---
+++ LC_796/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() == 1) {
+            return true ;
+        }
+        if (s.length() != goal.length()) {
+            return false ;
+        }
+
+        int count = 0 ;
+        while (count < s.length()) {
+            s = s.substr(1) + s[0] ;
+            if (s == goal) {
+                return true ;
+            }
+            count++ ;
+        }
+        return false ;
+    }
+};
