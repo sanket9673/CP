bac47157e339715b070d9cdeffe6592b8d3eed1e
sanket9673 <sanketch9673@gmail.com>
2024-08-27 17:56:05 +0530

    Check if array is sorted and rotated

---
+++ LC_1752/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size() ;
+        if (n < 3) {
+            return true ;
+        }
+        vector<int> arr(nums) ;
+        // arr = nums ;
+        sort(arr.begin(), arr.end()) ;
+        while (n >= 0) {
+            int temp = arr[0] ;
+            arr.erase(arr.begin()) ;
+            arr.push_back(temp) ;
+            if (arr == nums) {
+                return true ;
+            }
+            n-- ;
+        }
+        return false ;
+    }
+};
