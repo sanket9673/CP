b1f3a8e7eebd8cfde0d11791aa92a7875d25c20f
sanket9673 <sanketch9673@gmail.com>
2024-09-01 21:58:59 +0530

    Convert 1D to 2D

--- LC_2022/main.cpp
+++ LC_2022/main.cpp
@@ -6,6 +6,7 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         vector<vector<int>> res(m, vector<int>(n, 0));
+        
         if (original.size() == m * n) {
             int p = 0 ;
             for (int j = 0; j < m && p < original.size() ; j++) {
