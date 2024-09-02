35ccc4ea70c38a546a363cf792138a4247d9be10
sanket9673 <sanketch9673@gmail.com>
2024-09-02 22:00:58 +0530

    Find the student that will replace the chalk

--- LC_1894/main.cpp
+++ LC_1894/main.cpp
@@ -7,13 +7,14 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int count = 0 ;
         long long sum = 0 ;
-        
+
         while (count < chalk.size()) {
             sum += chalk[count] ;
             count++ ;
         }
         k %= sum ;
         count = 0 ;
+        
         while (true) {
             if (k < chalk[count]) {
                 break ;
