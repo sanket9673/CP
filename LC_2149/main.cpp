1918865f81b5857606eb30c7d4960575b6fb3491
sanket9673 <sanketch9673@gmail.com>
2024-09-02 22:46:44 +0530

    Rearrange Array elements by sign

---
+++ LC_2149/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> res ;
+        vector<int> pos ;
+        vector<int> neg ;
+
+        for (int i = 0 ; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                pos.push_back(nums[i]) ;
+            }
+            else {
+                neg.push_back(nums[i]) ;
+            }
+        }
+
+        for (int i = 0; i < nums.size() / 2; i++) {
+            res.push_back(pos[i]) ;
+            res.push_back(neg[i]) ;
+        }
+
+        return res ;
+    }
+};
