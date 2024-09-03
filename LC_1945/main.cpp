6ca2b9e32a0bd09ec8dfc942357d9aa29a7da944
sanket9673 <sanketch9673@gmail.com>
2024-09-03 21:32:03 +0530

    Sum of digit of string after convert

---
+++ LC_1945/main.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string arr = "" ;
+        for (int i = 0; i < s.length(); i++) {
+            arr += to_string(s[i] - 'a' + 1) ;
+        }
+        int res = 0 ;
+        for (int i = 0; i < arr.size(); i++) {
+            res += arr[i] - '0' ;
+        }
+        k -= 1 ;
+        if (k > 0) {
+            while (k >= 1) {
+                int r = 0;
+                while (res > 0) {
+                    r += res % 10 ;
+                    res /= 10 ;
+                }
+                res = r ;
+                k-- ;
+            }
+            return res ;
+        }
+        else {
+            return res ;
+        }
+        
+    }
+};
