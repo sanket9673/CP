e44a53efc969ae8825297110fe43feb8a243f529
sanket9673 <sanketch9673@gmail.com>
2024-09-03 21:33:21 +0530

    Sum of digit of string after convert

--- LC_1945/main.cpp
+++ LC_1945/main.cpp
@@ -9,13 +9,16 @@ public:
         for (int i = 0; i < s.length(); i++) {
             arr += to_string(s[i] - 'a' + 1) ;
         }
-        
+
         int res = 0 ;
         for (int i = 0; i < arr.size(); i++) {
             res += arr[i] - '0' ;
         }
+
         k -= 1 ;
+
         if (k > 0) {
+            
             while (k >= 1) {
                 int r = 0;
                 while (res > 0) {
@@ -27,6 +30,7 @@ public:
             }
             return res ;
         }
+
         else {
             return res ;
         }
