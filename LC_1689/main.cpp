8dce0c4cc7b6f93d00c2cb1aca0ee79a16ce12ec
sanket9673 <sanketch9673@gmail.com>
2024-09-12 19:39:30 +0530

    Count the number of consistent string

--- LC_1689/main.cpp
+++ LC_1689/main.cpp
@@ -7,9 +7,11 @@ public:
     int countConsistentStrings(string allowed, vector<string>& words) {
         int arr[256] ;
         int res = 0 ;
+
         for (int i = 0; i < allowed.length(); i++) {
             arr[allowed[i]]++ ;
         }
+        
         int count = 0 ;
         int present = 0 ;
         while(count < words.size()) {
