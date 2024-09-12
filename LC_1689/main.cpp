1c7fc7cc46296beba58849df63c736105cc624e2
sanket9673 <sanketch9673@gmail.com>
2024-09-12 19:38:09 +0530

    Count the number of consistent string

---
+++ LC_1689/main.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int arr[256] ;
+        int res = 0 ;
+        for (int i = 0; i < allowed.length(); i++) {
+            arr[allowed[i]]++ ;
+        }
+        int count = 0 ;
+        int present = 0 ;
+        while(count < words.size()) {
+            for (int i = 0; i < words[count].length(); i++) {
+                if (arr[words[count][i]] == 0) {
+                    break ;
+                }
+                else {
+                    present++ ;
+                }
+            }
+            if (present == words[count].length()) {
+                res++ ;
+            }
+            present = 0 ;
+            count++ ;
+        }
+
+        return res ;
+    }
+};
