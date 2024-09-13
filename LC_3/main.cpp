0be4e7479937837715a759db3e438d36fbe928eb
sanket9673 <sanketch9673@gmail.com>
2024-09-13 23:08:21 +0530

    Longest substring without repeating characters

---
+++ LC_3/main.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.length() < 2) {
+            return s.length() ;
+        }
+        int res = 0 ;
+        for (int i = 0; i < s.length(); i++) {
+            bool arr[256] = {false} ;
+            for (int j = i; j < s.length(); j++) {
+                if (arr[s[j]] == true) {
+                    break ;
+                }
+                else {
+                    res = max(res, j-i+1) ;
+                    arr[s[j]] = true ;
+                }
+            }
+        }
+        return res ;
+
+
+
+
+
+    //     int res = 0 ;
+    //     int old = 0 ;
+    //     bool arr[256] = {false} ;
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (arr[s[i]] == false) {
+    //             arr[s[i]] = true ;
+    //             old++ ;
+    //         }
+    //         else {
+    //             old = 0 ;
+    //             arr[s[i]] = false ;
+    //         }
+    //         res = max(old, res);
+    //     }
+    //     return res ;
+    }
+};
+
