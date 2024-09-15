d3a4b0f6bba18fbde557c11d997f3709899c9cb0
sanket9673 <sanketch9673@gmail.com>
2024-09-15 18:45:38 +0530

    Find the winning player in the coin game

---
+++ LC_3222/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string losingPlayer(int x, int y) {
+        int count = 0 ;
+        while (x > 0 && y > 3) {
+            x -= 1 ;
+            y -= 4 ;
+            count++ ;
+        }
+        if (count % 2 != 0) {
+            return "Alice" ;
+        }
+        else {
+            return "Bob" ;
+        }
+    }
+};
