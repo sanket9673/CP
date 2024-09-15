f4da4b85960e1cde0e69e540ae1cab436cd24cbc
sanket9673 <sanketch9673@gmail.com>
2024-09-15 18:38:41 +0530

    Longest substring with maximum bitwise AND

---
+++ LC_2419/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int res = 0 ; 
+        int count = 0 ;
+        int k = 0 ;
+        for (int i = 0; i < nums.size(); i++) {
+            k = max(k, nums[i]) ;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == k) {
+                count++ ;
+                res = max(res, count) ;
+            }
+            else {
+                count = 0 ;
+            }
+        }
+
+        return res ;
+    }
+};
