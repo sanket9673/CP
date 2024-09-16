8814539d39066d27b8cc249cf517b70561ff0733
sanket9673 <sanketch9673@gmail.com>
2024-09-17 03:13:14 +0530

    Top k frequent words

---
+++ LC_692/main.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool compare(const pair<string, int> a, const pair<string, int> b) {
+        if (a.second == b.second) {
+            return a.first < b.first ;
+        }
+        return a.second > b.second ;
+    }
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> x ;
+        for (const auto& word : words) {
+            if (x.count(word) == 0) {
+                x[word] = 1 ;
+            }
+            else {
+                x[word]++ ;
+            }
+        }
+
+        vector<string> res ;
+        vector<pair<string, int>> vec(x.begin(), x.end()) ;
+        sort(vec.begin(), vec.end(), compare) ;
+        int sz = 0 ;
+        for (const auto& pair : vec) {
+            res.push_back(pair.first) ;
+            sz++ ;
+            if (sz == k) {
+                break ;
+            }
+        }
+        return res ;
+    }
+};
