68c10173b416dc4eae72d97cc2622d2b90400299
sanket9673 <sanketch9673@gmail.com>
2024-09-16 21:42:01 +0530

    First Unique character of String

---
+++ LC_387/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if (s.length() < 2) {
+            return 0 ;
+        }
+
+        unordered_map<char, int> frq ;
+        unordered_map<char, int> idx ;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (frq.count(s[i]) == 0) {
+                frq[s[i]] = 1 ;
+                idx[s[i]] = i ;
+            }
+            else {
+                frq[s[i]]++ ;
+            }
+        }
+        int res = INT_MAX ;
+        for (int i = 0; i < s.length(); i++) {
+            if (frq[s[i]] == 1) {
+                res = min(idx[s[i]], res) ;
+            }
+        }
+
+        if (res == INT_MAX) {
+            return -1;
+        }
+        return res ;
+        
+    }
+};
