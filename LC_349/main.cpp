cd92011b6fa2fe5fded6a7e38c08c68620102d55
sanket9673 <sanketch9673@gmail.com>
2024-09-17 21:42:05 +0530

    Intersection of two arrays

---
+++ LC_349/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> x ;
+        unordered_set<int> y ;
+        for (int i = 0 ; i < nums1.size(); i++) {
+            x.insert(nums1[i]) ;
+        }
+        for (int i = 0 ; i < nums2.size(); i++) {
+            y.insert(nums2[i]) ;
+        }
+
+        vector<int> res ;
+        for (const auto& pair : y) {
+            if (x.count(pair) == 1) {
+                res.push_back(pair) ;
+            }
+        }
+
+        return res ;
+    }
+};
