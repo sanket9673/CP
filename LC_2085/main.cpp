d70cefc3463d588885bef1a30edf167500262933
sanket9673 <sanketch9673@gmail.com>
2024-09-17 21:34:25 +0530

    Count common words in one occurence

---
+++ LC_2085/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string, int> x ;
+        unordered_map<string, int> y ;
+        for (const string& word : words1) {
+            x[word]++;
+        }
+
+        for (const string& word : words2) {
+            y[word]++;
+        }
+
+        int res = 0 ;
+        for (const auto& pair : x) {
+            if (pair.second == 1 && y[pair.first] == 1) {
+                res++;
+            }
+        }
+
+        return res ;
+    }
+};
