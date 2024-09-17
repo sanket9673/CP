00441c87aab34cbb99c5fe36e1f170cc01ae169f
sanket9673 <sanketch9673@gmail.com>
2024-09-17 21:14:39 +0530

    Uncommon Words from two strings

---
+++ LC_884/main.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        stack<string> stk1, stk2 ;
+        string res = "" ;
+        for(int i = 0; i < s1.length(); i++) {
+            if(isalpha(s1[i])) {
+                res+= s1[i] ;
+            }
+            else {
+                stk1.push(res) ;
+                res = "" ;
+            }
+        }
+
+        if(!res.empty()) {
+            stk1.push(res) ;
+            res = "" ;
+        }
+
+        for(int i = 0; i < s2.length(); i++) {
+            if(isalpha(s2[i])) {
+                res+= s2[i] ;
+            }
+            else {
+                stk2.push(res) ;
+                res = "" ;
+            }
+        }
+
+        if(!res.empty()) {
+            stk2.push(res) ;
+            res = "" ;
+        }
+
+        unordered_map<string, int> x ;
+        while (!stk1.empty()) {
+            x[stk1.top()]++ ;
+            stk1.pop();
+        }
+        while (!stk2.empty()) {
+            x[stk2.top()]++ ;
+            stk2.pop();
+        }
+
+        vector<string> result ;
+        for (const auto& pair : x) {
+            if(pair.second == 1) {
+                result.push_back(pair.first) ;
+            }
+        }
+        return result ;
+    }
+};
