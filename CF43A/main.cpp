6a54d1616a11c09cee3237a87494eefcee88fe8c
sanket9673 <sanketch9673@gmail.com>
2024-09-20 02:35:22 +0530

    Football

--- CF43A/main.cpp
+++ CF43A/main.cpp
@@ -1,92 +1,26 @@
 #include <iostream>
-#include <string>
-using namespace std ;
-
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+ 
 int main() {
-    int n; 
-    int count1 = 0, count2 = 0, count3 = 0;
+    int n ;
     cin >> n ;
-    string arr[n] ;
-    string s ;
-    
+    unordered_map<string, int> x ;
     for (int i = 0; i < n; i++) {
-        cin >> arr[i] ;
+        string team ;
+        cin >> team ;
+        x[team]++ ;
     }
-    
-    for (int i = 0; i < n; i++) {
-        s = arr[i] ;
-        for (int j = 0; j < s.size(); j++) {
-            if (s[j] == 'A') {
-                count1++ ;
-            }
-            else if (s[j] == 'B') {
-                count2++ ;
-            }
-            else if (s[j] == 'C') {
-                count3++ ;
-            }
+    int maxgoal = 0 ;
+    string win ;
+    for (const auto& pair : x) {
+        if (pair.second > maxgoal) {
+            win = pair.first ;
+            maxgoal = pair.second ;
         }
     }
-    
-    if (count1 > count2 && count1 > count3) {
-        cout << 'A' ;
-    }
-    else if (count2 > count1 && count2 > count3) {
-        cout << 'B' ;
-    }
-    else if (count3 > count1 && count3 > count2) {
-        cout << 'C' ;
-    }
-    else {
-        cout << "ABC" ;
-    }
-    
-    return 0 ;
+    cout << win ;
+    return 0 ; 
 }
-
-// #include <iostream>
-// #include <cmath>
-// using namespace std ;
-
-// int main() {
-//     int n ;
-//     cin >> n ;
-//     int count1 = 0, count2 = 0, count3 = 0;
-//     string arr[n] ;
-//     string s ;
-    
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i] ;
-//     }
-    
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < arr[i].size(); j++) {
-//             if (arr[i][j] == 'A') {
-//                 count1++ ;
-//             }
-//             else if (arr[i][j] == 'B') {
-//                 count2++ ;
-//             }
-//             else if (arr[i][j] == 'C') {
-//                 count3++ ;
-//             }
-//         }
-//     }
-    
-//     cout << count1 << count2 << count3 << endl;
-    
-//     if (count1 == count2 && count2 == count3) {
-//         cout << "ABC" ;
-//     }
-//     else if (count1 > count2 && count1 > count3) {
-//         cout << 'A' ;
-//     }
-//     else if (count2 > count1 && count2 > count3) {
-//         cout << 'B' ;
-//     }
-//     else {
-//         cout << 'C' ;
-//     }
-    
-//     return 0 ;
-// }
