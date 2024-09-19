65938bb49c8f4fb0f69e2eee1c2cdce5b33490c3
sanket9673 <sanketch9673@gmail.com>
2024-09-20 01:53:58 +0530

    Sale

--- CF34B/main.cpp
+++ CF34B/main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std ;
+using namespace std;
 
 int main() {
-    vector<int> arr ;
-    int res = 0 ;
-    int n, m ;
-    cin >> n >> m ;
-    for (int i = 0 ; i < n; i++) {
-        cin >> arr[i] ;
+    int res = 0;
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
     
-    for (int i = 0 ; i < m; i++) {
-        res+= arr[i] ;
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < m; i++) {
+        if (arr[i] < 0) {
+            res += abs(arr[i]);  
+        }
     }
-    
-    return res ;
+
+    cout << res << endl; 
+    return 0;
 }
