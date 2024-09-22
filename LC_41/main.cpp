62c37f6a5db370581acf407da53df95c79b6cef3
sanket9673 <sanketch9673@gmail.com>
2024-09-22 23:34:00 +0530

    First Missing Positive

---
+++ LC_41/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<long long> x(nums.begin(), nums.end()) ;
+        // int mx = 0 ;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (x.count(nums[i]) == 0) {
+        //         x.insert(nums[i]) ;
+        //     }
+        // }
+
+        long long res = 0; 
+        int i = 1 ;
+        while (true) {
+            if (x.count(i) == 0) {
+                res = i ;
+                break ;
+            }
+            i++ ;
+        }
+        return res ;
+    }
+};
