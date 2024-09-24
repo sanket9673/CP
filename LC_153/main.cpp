49a6da32b6c915bcef40cf6e8dc0de94dd52e8d8
sanket9673 <sanketch9673@gmail.com>
2024-09-24 12:58:35 +0530

    Find the Minimum in rotated sorted array

--- LC_153/main.cpp
+++ LC_153/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std ;
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -14,8 +9,32 @@ public:
         //     mn = min(mn, nums[i]) ;
         // }
         // return mn ;
-        // The above Code written with time complexity O(n) 
+        if (nums.size() < 2) {
+            return nums[0] ;
+        }
+        int low = 0 ;
+        int high = nums.size() - 1 ;
+        int res = 0 ;
+        if (nums[low] <= nums[high]) {
+            return nums[low] ; //Means they are sorted and not rotated
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2 ;
 
-        
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid] ;
+            }
+            if (mid < nums.size() - 1 && nums[mid] > nums[mid+1]) {
+                return nums[mid+1] ;
+            }
+            // The above is used to handle the edge test cases  .
+            if (nums[low] <= nums[mid]) {
+                low = mid + 1 ; // Sorted so they are in increasing order, must lie in right
+            }
+            if (nums[mid] <= nums[high]) {
+                high = mid - 1 ; // Sorted so they are in increasing order, must lie in left
+            }
+        }
+        return -1 ;
     }
 };
