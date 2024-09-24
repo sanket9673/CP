5d715e81931731cf79503b058459eebfe83e6c1d
sanket9673 <sanketch9673@gmail.com>
2024-09-24 12:17:42 +0530

    Find the Minimum in rotated sorted array

--- LC_153/main.cpp
+++ LC_153/main.cpp
@@ -6,13 +6,16 @@ using namespace std ;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        if (nums.size() < 2) {
-            return nums[0] ;
-        }
-        int mn = nums[0] ;
-        for (int i = 1; i < nums.size(); i++) {
-            mn = min(mn, nums[i]) ;
-        }
-        return mn ;
+        // if (nums.size() < 2) {
+        //     return nums[0] ;
+        // }
+        // int mn = nums[0] ;
+        // for (int i = 1; i < nums.size(); i++) {
+        //     mn = min(mn, nums[i]) ;
+        // }
+        // return mn ;
+        // The above Code written with time complexity O(n) 
+
+        
     }
 };
