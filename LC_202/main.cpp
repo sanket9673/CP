b7f31c747b88ca4ef2987af89aecc6851903e008
sanket9673 <sanketch9673@gmail.com>
2024-09-25 17:35:34 +0530

    Happy Number

---
+++ LC_202/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<long long> x ;
+        while (x.count(n) == 0) {
+            x.insert(n) ;
+            int sum = 0 ;
+            while(n > 0) {
+                int a = n % 10 ;
+                sum += a * a ;
+                n = n / 10 ;
+            }
+            if (sum == 1) {
+                return true ;
+            }
+            n = sum ;
+        } 
+        return false ;
+    }
+};
