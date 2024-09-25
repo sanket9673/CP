4869c9cf32d0dcb9b5e302f389cded2ec651ec61
sanket9673 <sanketch9673@gmail.com>
2024-09-25 17:56:50 +0530

    Ugly Number

--- LC_263/main.cpp
+++ LC_263/main.cpp
@@ -1,26 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std ;
-
 class Solution {
 public:
     bool isUgly(int n) {
+        unordered_set<int> x = {2,3,5} ;
+        vector<long long> fact ;
         if (n <= 0) {
             return false;
         }
-        if (n == 1) {
-            return true;
-        }
+
         while (n % 2 == 0) {
-            n /= 2;
+            n = n / 2 ;
         }
-        while (n % 3 == 0) {
-            n /= 3;
+        if (n == 1) {
+            return true ;
         }
-        while (n % 5 == 0) {
-            n /= 5;
+
+        for (long long i = 3; i*i <= n; i+=2) {
+            while(n % i == 0) {
+                if (x.count(i) == 0) {
+                    return false ;
+                }
+                n = n / i ;
+            }
         }
-        return n == 1;
+        
+        if (n == 1) {
+            return true ;
+        }
+        if(n > 2) {
+            if (x.count(n) == 0) {
+                return false ;
+            }
+        }
+        return true ;
+
     }
 };
