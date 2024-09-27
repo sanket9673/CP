b18428a3fdeb3a78ffa80d2920be4850c178288b
sanket9673 <sanketch9673@gmail.com>
2024-09-27 22:46:55 +0530

    Count the digit that divides the number

---
+++ LC_2520/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count = 0 ;
+        int n = num;
+        while (n > 0) {
+            int val = n % 10 ;
+            if (num % val == 0) {
+                count++ ;
+            }
+            n /= 10 ;
+        }
+        return count ;
+    }
+};
