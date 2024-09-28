0338eb3e05913f44468e88ba9a93c5833c137577
sanket9673 <sanketch9673@gmail.com>
2024-09-29 02:13:47 +0530

    Maximize the total height of unique towers

---
+++ LC_3301/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maximumTotalSum(vector<int>& maximumHeight) {
+        if (maximumHeight.size() < 2) {
+            return maximumHeight[0] ;
+        }
+        
+        sort(maximumHeight.begin(), maximumHeight.end(), greater<long>());
+        long long prev = INT_MAX;
+        long long total = 0;
+
+        for(int i = 0; i<maximumHeight.size(); i++){
+            if (maximumHeight[i] < prev){
+                prev = maximumHeight[i];
+                total += prev;
+            }
+            else{
+                prev -= 1;
+                if (prev <= 0){
+                    return -1;
+                }
+                total += prev;
+            }
+        }
+        return total;
+    }
+};
