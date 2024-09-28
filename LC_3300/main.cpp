62ef98135f121f62c12f2454073f6949efb74468
sanket9673 <sanketch9673@gmail.com>
2024-09-28 23:28:00 +0530

    Minimum Element after replacement with digit sum

---
+++ LC_3300/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        vector<int> res ;
+        for(int i = 0 ; i < nums.size(); i++) {
+            int sum  = 0 ;
+            while(nums[i] > 0) {
+                sum += nums[i] % 10 ;
+                nums[i] /= 10 ;
+            }
+            res.push_back(sum) ;
+        }
+        int mn = INT_MAX ;
+        for (int val : res) {
+            mn = min(val , mn) ;
+        }
+
+        return mn ;
+    }
+};
