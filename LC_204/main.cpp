8eaf3b14f205eaf84e466a5f4e11d515793fe70d
sanket9673 <sanketch9673@gmail.com>
2024-09-28 16:45:47 +0530

    Count Prime

--- LC_204/main.cpp
+++ LC_204/main.cpp
@@ -10,18 +10,33 @@ public:
         }
         int count = 0 ;
         vector<bool> isprime(n, true) ;
+        // for (int i = 2; i*i < n; i++) {
+        //     if(isprime[i] == true) {
+        //         for(int j = 2*i; j < n; j+=i) {
+        //             isprime[j] = false ;
+        //         }
+        //     }
+        // }
+        // for(int i = 2; i < n; i++) {
+        //     if(isprime[i] == true) {
+        //         count++ ;
+        //     }
+        // }
+        // return count ;
+
         for (int i = 2; i*i < n; i++) {
-            if(isprime[i] == true) {
-                for(int j = 2*i; j < n; j+=i) {
+            if (isprime[i] == true) {
+                for (int j = i*i; j < n ; j+=i) {
                     isprime[j] = false ;
                 }
             }
         }
-        for(int i = 2; i < n; i++) {
-            if(isprime[i] == true) {
-                count++ ;
+        for (int i = 2; i < n; i++) {
+            if (isprime[i]) {
+                count++;
             }
         }
-        return count ;
+
+        return count;
     }
 };
