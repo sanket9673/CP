cbaf0522fe0fe9d0ffe250decaea9804dee87080
sanket9673 <sanketch9673@gmail.com>
2024-09-29 17:21:49 +0530

    Search in left Rotated Sorted Array II

--- LC_81/main.cpp
+++ LC_81/main.cpp
@@ -7,6 +7,7 @@ public:
     bool search(vector<int>& nums, int target) {
         int l = 0 ;
         int r = nums.size() - 1 ;
+        
         while (l <= r) {
             int mid = l + (r - l) / 2 ;
             if (nums[mid] == target) {
