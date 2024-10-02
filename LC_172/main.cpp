08f3d932cd85b167cb5618a27afef2e3069f519a
sanket9673 <sanketch9673@gmail.com>
2024-10-03 05:06:04 +0530

    Factorial Trailing Zeros

---
+++ LC_172/main.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        // long long fact = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     fact *= i;
+        // }
+        // long long count = 0 ;
+        // string s = to_string(fact) ;
+        // for (int i = s.length() - 1; i >= 0; i--) {
+        //     if (s[i] == '0') {
+        //         count++ ;
+        //     }
+        //     else {
+        //         break ;
+        //     }
+        // } 
+        // return count ;    
+        // LEADING TO RUNTIME ERROR 
+
+        // int fact = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     fact *= i;
+        // }
+        // int count2 = 0, count5 = 0 ;
+        // while (fact % 2 == 0) {
+        //     count2++ ;
+        //     fact /= 2 ;
+        // }
+        // while (fact % 5 == 0) {
+        //     count5++ ;
+        //     fact /= 5 ;
+        // }
+        // return min(count2, count5) ;
+        // // LEADING TO RUNTIME ERROR 
+
+        int res = 0 ;
+        for (int i = 5; i <= n; i*=5)  {
+            res += (n / i) ;
+        }
+        return res ;
+    }
+};
