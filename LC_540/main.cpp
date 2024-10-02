76f2fc4adf0147d45905f1206e8316043db24e02
sanket9673 <sanketch9673@gmail.com>
2024-10-02 18:55:06 +0530

    Single Element in Sorted Array

---
+++ LC_540/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if (nums.size() % 2 == 0) {
+            return -1 ;
+        }
+        else if (nums.size() < 2) {
+            return nums[0] ;
+        }
+        int l = 0 ;
+        int r = nums.size() - 1 ;
+        while (l < r) {
+            int mid = l + (r - l) / 2 ;
+            if (mid % 2 == 0 && mid < nums.size() - 1) {
+                if (nums[mid] == nums[mid + 1]) {
+                    l = mid + 1 ;
+                }
+                else {
+                    r = mid;
+                }
+            }
+            else if (mid % 2 != 0 && mid > 0) {
+                if (nums[mid] == nums[mid - 1]) {
+                    l = mid + 1 ;
+                }
+                else {
+                    r = mid-1 ;
+                }
+            }
+        }
+        return nums[l] ;
+    }
+};
