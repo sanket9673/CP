d176cc37eba5e5cfd0105a3efcb2994b7f6a1595
sanket9673 <sanketch9673@gmail.com>
2024-10-03 05:44:24 +0530

    Rotate Array

---
+++ LC_189/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // normaliziing k to handle the greater value
+        int n = nums.size() ;
+        k %= n ;
+        vector<int> res(n) ;
+        for (int i = 0; i < k ; i++) {
+            res[i] = nums[n - k + i] ; 
+        }
+        for (int i = 0; i < n - k; i++) {
+            res[i+k] = nums[i] ;
+        }
+        nums = res ;
+    }
+};
