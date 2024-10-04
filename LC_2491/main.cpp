178bdad479f6a21ede3cac07b9cd447aecce9312
sanket9673 <sanketch9673@gmail.com>
2024-10-04 20:23:17 +0530

    Divide Players into  Teams of Equal Skill

---
+++ LC_2491/main.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(), skill.end()) ;
+        int p = 0 ;
+        int q = skill.size() - 1 ;
+        int sum = skill[p] + skill[q];
+        if (skill.size() < 3) {
+            return skill[p] * skill[q] ;
+        }
+        // unordered_map<long long, int> x ;
+        // while (p < q) {
+        //     if (skill[p] + skill[q] != sum) {
+        //         return -1 ;
+        //     }
+        //     if (x.count(skill[p]) == 0) {
+        //         x.insert({skill[p] * skill[q], 1}) ;
+        //     }
+        //     else {
+        //         x[skill[p] * skill[q]]++ ;
+        //     }
+            
+        //     p++ ;
+        //     q-- ;
+        // }
+        // long long res = 0 ;
+        // for (const auto& pair : x) {
+        //     res += (pair.first) * (pair.second) ;
+        // }
+        // return res ;
+        long long res = 0 ;
+        while (p < q) {
+            if (skill[p] + skill[q] != sum) {
+                return -1;
+            } 
+            res += skill[p] * skill[q] ;
+            p++ ;
+            q-- ;
+        }
+        return res ;
+    }
+};
