8c348324cb4864f8c71e69aa1296f671cbaec126
sanket9673 <sanketch9673@gmail.com>
2024-10-05 15:19:28 +0530

    Permutation in String

---
+++ LC_567/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) {
+            return false ;
+        }
+        sort(s1.begin(), s1.end()) ;
+        for (int i = 0 ; i < s2.length(); i++) {
+            string res = s2.substr(i, s1.length()) ;
+            sort(res.begin(), res.end()) ;
+            if (s1 == res) {
+                return true ;
+            }
+        }
+        return false ;
+    }
+};
