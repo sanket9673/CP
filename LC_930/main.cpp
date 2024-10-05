6d31949d10a901a23c213f30e9620c4192b4ffe1
sanket9673 <sanketch9673@gmail.com>
2024-10-05 19:51:57 +0530

    Binary Subarray with Sum

---
+++ LC_930/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static int findingSubarrays(vector<int>& nums, int goal) {
+        if (goal < 0) {
+            goal = 0 ;
+        }
+        int l = 0 ;
+        int current = 0 ;
+        int sum = 0 ;
+        for (int i = 0 ; i < nums.size(); i++) {
+            current += nums[i] ;
+            while (current > goal) {
+                current -= nums[l] ;
+                l += 1 ;
+            }
+            sum += (i - l + 1) ;
+        }
+        return sum ;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal) {    
+        if (goal == 0) {
+            return findingSubarrays(nums, goal) ;    
+        }
+        return findingSubarrays(nums, goal) - findingSubarrays(nums, goal - 1) ;
+    }
+};
