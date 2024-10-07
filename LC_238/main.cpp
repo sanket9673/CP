e89150dbf65381d2f77005a5e167405804d56f75
sanket9673 <sanketch9673@gmail.com>
2024-10-08 03:15:11 +0530

    Product of array except self

--- LC_238/main.cpp
+++ LC_238/main.cpp
@@ -4,32 +4,44 @@ using namespace std ;
 
 class Solution {
 public:
-    static int zeroOccur(vector<int>& nums, int k) {
-        int prod = 1 ;
-        for (int i = 0; i < k; i++) {
-            prod *= nums[i] ;
-        }
-        for (int i = k + 1; i < nums.size(); i++) {
-            prod *= nums[i] ;
-        }
-        return prod ;
-    }
+    // static int zeroOccur(vector<int>& nums, int k) {
+    //     int prod = 1 ;
+    //     for (int i = 0; i < k; i++) {
+    //         prod *= nums[i] ;
+    //     }
+    //     for (int i = k + 1; i < nums.size(); i++) {
+    //         prod *= nums[i] ;
+    //     }
+    //     return prod ;
+    // }
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size() ;
-        int prod = 1 ;
-        for (int val : nums) {
-            prod *= val ;
+        // int n = nums.size() ;
+        // int prod = 1 ;
+        // for (int val : nums) {
+        //     prod *= val ;
+        // }
+        // vector<int> res ;
+        // for (int i = 0; i < n; i++) {
+        //     int val ;
+        //     if (nums[i] != 0) {
+        //         val = prod / nums[i] ;
+        //     }
+        //     else {
+        //         val = zeroOccur(nums, i) ;
+        //     }
+        //     res.push_back(val) ;
+        // }
+        // return res ;
+        int l = 1 ;
+        int r = 1 ;
+        vector<int> res(nums.size() , 1) ;
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] *= l ;
+            l *= nums[i] ;
         }
-        vector<int> res ;
-        for (int i = 0; i < n; i++) {
-            int val ;
-            if (nums[i] != 0) {
-                val = prod / nums[i] ;
-            }
-            else {
-                val = zeroOccur(nums, i) ;
-            }
-            res.push_back(val) ;
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            res[i] *= r ;
+            r *= nums[i] ;
         }
         return res ;
     }
