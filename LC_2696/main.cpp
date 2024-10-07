d1fef3100404cbdd687d1820a5e8f7d01ec5c472
sanket9673 <sanketch9673@gmail.com>
2024-10-07 19:58:34 +0530

    Minimum string length after removing substring

---
+++ LC_2696/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> x ;
+        for (int i = 0; i < s.length(); i++) {
+            if (x.empty()) {
+                x.push(s[i]) ;
+            }
+            else if (s[i] == 'B' && x.top() == 'A' || s[i] == 'D' && x.top() == 'C') {
+                x.pop() ;
+            }
+            else {
+                x.push(s[i]) ;
+            }
+        }
+        // int count = 0 ;
+        // while (!x.empty()) {
+        //     x.pop() ;
+        //     count++ ;
+        // }
+        // return count ;
+        return x.size() ;
+    }
+};
