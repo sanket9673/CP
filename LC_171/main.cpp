3f60f739fe8ec0116b56fc35cd767a811d65c28f
sanket9673 <sanketch9673@gmail.com>
2024-10-10 01:17:24 +0530

    Excel Sheet Column Number

---
+++ LC_171/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int n = columnTitle.length() ;
+        int res = 0 ;
+        for (char c : columnTitle) {
+            n -= 1 ;
+            res += (c - 64)*(pow(26, n)) ;
+        }
+        return res ;
+    }
+};
