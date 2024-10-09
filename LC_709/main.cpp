e85e4fe55516c165147604b8c989285a6d26fe7d
sanket9673 <sanketch9673@gmail.com>
2024-10-09 22:39:27 +0530

    To lower case

---
+++ LC_709/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string res = "" ;
+        for (int i = 0 ; i < s.length(); i++) {
+            char c = tolower(s[i]);
+            res += c ;
+        }
+        return res ;
+    }
+};
