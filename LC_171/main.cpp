811d18254fecf8a84244ba8293841078164f22bf
sanket9673 <sanketch9673@gmail.com>
2024-10-10 23:16:40 +0530

    Excel Sheet Column Number

--- LC_171/main.cpp
+++ LC_171/main.cpp
@@ -5,13 +5,14 @@ using namespace std ;
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
+        
         int n = columnTitle.length() ;
         int res = 0 ;
         for (char c : columnTitle) {
             n -= 1 ;
             res += (c - 64)*(pow(26, n)) ;
         }
-        
+
         return res ;
     }
 };
