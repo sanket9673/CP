23536ca085313dd193e86fae9c2aa2ec25416be0
sanket9673 <sanketch9673@gmail.com>
2024-10-11 04:21:51 +0530

    Sum of Square Numbers

---
+++ LC_633/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int n = (int)sqrt(c) ;
+        if (n*n == c){
+            return true ;
+        } 
+        else {
+            int l = 0 ;
+            int r = n ;
+            while (l <= r) {
+                long long sum = (long long)l * l + (long long)r * r ;
+                if (sum == c) {
+                    return true ;
+                }
+                else if (sum > c) {
+                    r-- ;
+                }
+                else {
+                    l++ ;
+                }
+            }
+            return false ;
+        }
+    }
+};
