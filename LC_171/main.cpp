58bea5c476cf15bc501a74c1387321857ca5d5f3
sanket9673 <sanketch9673@gmail.com>
2024-10-10 23:16:49 +0530

    Excel Sheet Column Number

--- LC_171/main.cpp
+++ LC_171/main.cpp
@@ -5,9 +5,10 @@ using namespace std ;
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        
+
         int n = columnTitle.length() ;
         int res = 0 ;
+        
         for (char c : columnTitle) {
             n -= 1 ;
             res += (c - 64)*(pow(26, n)) ;
