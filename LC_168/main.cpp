cf215b0ee2a8f2ba42136f553749ed77b521e836
sanket9673 <sanketch9673@gmail.com>
2024-10-11 00:16:37 +0530

    Excel Sheet Column Title

---
+++ LC_168/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        int n = columnNumber ;
+        string res = "";
+        while (n > 0) {
+            if (n % 26 == 0) {
+                res += 'Z' ;
+                n = (n/26) - 1 ; //Representing a full cycle is completed 
+            }
+            else {
+                res += (char)((n % 26) + 64);
+                n /= 26; 
+            }
+        }
+        reverse(res.begin(), res.end()) ;
+        return res ;
+    }
+};
