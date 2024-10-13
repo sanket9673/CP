922e806fc501c8687171d2fa92219944ca53ba38
sanket9673 <sanketch9673@gmail.com>
2024-10-13 06:53:50 +0530

    Subarray sum equal to k

---
+++ LC_560/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> x ;
+        x[0] = 1 ;
+        int sum, count = 0 ; 
+        for (int i = 0 ; i < nums.size(); i++) {
+            sum += nums[i] ;
+            if (x.count(sum - k) != 0) {
+                count += x[sum - k] ; // Adding all the frequency occurence of (sum - k)
+            }
+            x[sum]++ ;
+        }
+        return count ;
+    }
+};
