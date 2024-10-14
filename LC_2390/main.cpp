8acc638708a174230637a80afbbfcf6c34de72da
sanket9673 <sanketch9673@gmail.com>
2024-10-14 13:19:35 +0530

    Remove Stars from string

--- LC_2390/main.cpp
+++ LC_2390/main.cpp
@@ -6,25 +6,35 @@ using namespace std ;
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> x ;
+        // stack<char> x ;
+        // string res = "" ;
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (x.empty() && s[i] != '*') {
+        //         x.push(s[i]) ;
+        //     }
+        //     else if (s[i] != '*') {
+        //         x.push(s[i]) ;
+        //     }
+        //     else if (s[i] == '*' && !x.empty()) {
+        //         x.pop() ;
+        //     }
+        // }
+        // while (!x.empty()) {
+        //     char ans = x.top() ;
+        //     res += ans ;
+        //     x.pop() ;
+        // }
+        // reverse(res.begin(), res.end()) ;
+        // return res ;
         string res = "" ;
-        for (int i = 0; i < s.length(); i++) {
-            if (x.empty() && s[i] != '*') {
-                x.push(s[i]) ;
+        for (int i = 0 ; i < s.length(); i++) {
+            if (s[i] != '*') {
+                res.push_back(s[i]) ;
             }
-            else if (s[i] != '*') {
-                x.push(s[i]) ;
-            }
-            else if (s[i] == '*' && !x.empty()) {
-                x.pop() ;
+            else if (!res.empty()){
+                res.pop_back() ;
             }
         }
-        while (!x.empty()) {
-            char ans = x.top() ;
-            res += ans ;
-            x.pop() ;
-        }
-        reverse(res.begin(), res.end()) ;
-        return res ;
+        return res;
     }
 };
