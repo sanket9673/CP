92d7cb51e60380a138d517af4f66a2e34d0d22ad
sanket9673 <sanketch9673@gmail.com>
2024-10-15 04:50:38 +0530

    Find the Integer added to array I

---
+++ LC_3131/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        int a = INT_MAX ;
+        int b = INT_MAX ;
+        for (int val : nums1) {
+            a = min(val, a) ;
+        }
+        for (int val : nums2) {
+            b = min(val, b) ;
+        }
+        return b - a ;
+    }
+};
