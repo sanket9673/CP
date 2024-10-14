9f2fd3e204b750ff154f0202701b0689c4b34f36
sanket9673 <sanketch9673@gmail.com>
2024-10-15 01:25:20 +0530

    Minimum Element to make the array unique

---
+++ LC_945/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end()) ;
+        int res = 0 ;
+        int temp = nums[0] ;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= temp) {
+                int a = nums[i] ;
+                nums[i] = temp + 1 ;
+                res += nums[i] - a ;
+            }
+            temp = nums[i] ;
+        }
+        return res ;
+    }
+};
