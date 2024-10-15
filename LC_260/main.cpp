6bf23d3554984249d65ef97a199f3f7792275222
sanket9673 <sanketch9673@gmail.com>
2024-10-15 06:31:23 +0530

    Single Number III

---
+++ LC_260/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_set<int> x ;
+        for (int val : nums) {
+            if (x.count(val) == 0) {
+                x.insert(val) ;
+            }
+            else {
+                x.erase(val) ;
+            }
+        }
+        vector<int> res ;
+        for (const auto& pair : x) {
+            res.push_back(pair) ;
+        }
+        return res ;
+    }
+};
