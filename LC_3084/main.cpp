502e524d9139ee1f7b593741dd51e376e844f7dc
sanket9673 <sanketch9673@gmail.com>
2024-10-17 03:12:23 +0530

    Count Substring starting and ending with given character

---
+++ LC_3084/main.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countSubstrings(string s, char c) {
+        long long res = 0 ;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == c) {
+                res++ ;
+            }
+        }
+        return res*(res+1)/2 ;
+    }
+};
