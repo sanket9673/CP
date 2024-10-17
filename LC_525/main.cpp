edbf6610eab71f36f227d4efca4aa30dd5d295b5
sanket9673 <sanketch9673@gmail.com>
2024-10-17 21:06:42 +0530

    Contiguous Array

---
+++ LC_525/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                nums[i] = -1 ;
+            }
+        }
+        unordered_map<int, int> x ;
+        int res = 0 ;
+        int pref = 0 ;
+        x[0] = -1 ;
+        for (int i = 0 ; i < nums.size(); i++) {
+            pref += nums[i] ;
+            if (x.count(pref) != 0) {
+                res = max(res, i - x[pref]);
+            }
+            else {
+                x[pref] = i ;
+            }
+        }
+        return res ;
+    }
+};
