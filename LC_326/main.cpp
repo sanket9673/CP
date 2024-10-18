2cd717324bee86930890f7c23bbed0678c36c088
sanket9673 <sanketch9673@gmail.com>
2024-10-18 23:12:59 +0530

    Power of Three

---
+++ LC_326/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n < 1) {
+            return false ;
+        }
+        else if (n == 1) {
+            return true ;
+        }
+        else {
+            while (n % 3 == 0) {
+                n /= 3 ;
+            }
+            if (n == 1) {
+                return true ;
+            }
+            else {
+                return false ;
+            }
+        }
+    }
+};
