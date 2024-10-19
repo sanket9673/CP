4a97edc4ae6e8fc26ed7493fd06722c7641f572f
sanket9673 <sanketch9673@gmail.com>
2024-10-19 18:55:03 +0530

    Find the lucky number in array

---
+++ LC_1394/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> x ;
+        int res = INT_MIN ;
+        for (int i = 0 ; i < arr.size(); i++) {
+            x[arr[i]]++ ;
+        }
+        for (const auto& pair : x) {
+            if (pair.first == pair.second) {
+                res = max(res, pair.first) ;
+            }
+        }
+        
+        return (res != INT_MIN) ? res : -1 ;
+    }
+};
