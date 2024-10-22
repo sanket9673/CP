d5223ee19016bf30bff5031005128caffa264188
sanket9673 <sanketch9673@gmail.com>
2024-10-22 23:09:25 +0530

    Find the lucky

--- LC_1394/main.cpp
+++ LC_1394/main.cpp
@@ -9,10 +9,11 @@ public:
     int findLucky(vector<int>& arr) {
         unordered_map<int, int> x ;
         int res = INT_MIN ;
-        
+
         for (int i = 0 ; i < arr.size(); i++) {
             x[arr[i]]++ ;
         }
+        
         for (const auto& pair : x) {
             if (pair.first == pair.second) {
                 res = max(res, pair.first) ;
