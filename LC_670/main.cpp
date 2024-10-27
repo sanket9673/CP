4c651d3a8cd5789583f63b4b913d83e34d5c7816
sanket9673 <sanketch9673@gmail.com>
2024-10-27 09:03:34 +0530

    Maximum Swap

---
+++ LC_670/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string res = to_string(num) ;
+        int n = res.size() ;
+
+        vector<int> last(10, -1) ;
+        for (int i = 0 ; i < n; i++) {
+            last[res[i] - '0'] = i ;
+        }
+
+        for (int i = 0 ; i < n ; i++) {
+            for (int d = 9 ; d > res[i] - '0'; d--) {
+                if (last[d] > i) {
+                    swap(res[i], res[last[d]]) ;
+                    return stoi(res) ;
+                }
+            }
+        }
+        return num ;
+    }
+};
