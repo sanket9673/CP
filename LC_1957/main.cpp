c6a25be62f4e8fde6d8d66e1b857bbcfaa83e54c
sanket9673 <sanketch9673@gmail.com>
2024-11-01 18:05:33 +0530

    Delete character to make fancy string

---
+++ LC_1957/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        if (s.length() < 2) {
+            return s ;
+        }
+        string res = "" ;
+        char c = s[0] ;
+        res += c ;
+        int count = 1 ;
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == c) {
+                count++ ;
+                if (count < 3) {
+                    res += c ;
+                }
+            }
+            else {
+                c = s[i] ;
+                count = 1 ;
+                res += c ;
+            }
+        }
+        return res ;
+    }
+};
