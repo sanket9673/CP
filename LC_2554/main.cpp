ad9300195540b363d7d17e9605b3a9a05b9a0142
sanket9673 <sanketch9673@gmail.com>
2024-12-06 09:23:52 +0530

    Maximum number of integer to choose from range I

---
+++ LC_2554/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        unordered_set<int> x ;
+        for (int val : banned) {
+            x.insert(val) ;
+        }
+        vector<int> arr ;
+        for (int i = 1; i <= n; i++) {
+            if (x.count(i) == 0) {
+                arr.push_back(i) ;
+            }
+        }
+        int count = 0 ;
+        int sum = 0 ;
+        for (int i = 0; i < arr.size(); i++) {
+            if (sum + arr[i] <= maxSum) {
+                count++ ;
+                sum += arr[i] ;
+            }
+        }
+        return count ;
+    }
+};
