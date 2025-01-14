ddb0a59c5f5068e636d42ee8c1b64b5453fd2126
sanket9673 <sanketch9673@gmail.com>
2025-01-15 04:32:19 +0530

    Delete nth node from end

--- LC_19/main.cpp
+++ LC_19/main.cpp
@@ -8,6 +8,9 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include <iostream>
+using namespace std ;
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
