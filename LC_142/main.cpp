f749c81684f73db5bbe1bc8c322461b0afd9f5e9
sanket9673 <sanketch9673@gmail.com>
2025-01-14 23:50:13 +0530

    Linked List cycle II

---
+++ LC_142/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        // return NULL - No cycle
+        // return head/curr->next - cycle
+        unordered_set<ListNode*> x ;
+        ListNode* curr ;
+        if (head == NULL) {
+            return NULL ;
+        }
+        if (head -> next == NULL) {
+            return NULL ;
+        }
+        x.insert(head) ;
+        for (curr = head; curr != NULL; curr = curr -> next) {
+            if (x.count(curr -> next) == 1) {
+                return curr->next ;
+            }
+            x.insert(curr -> next) ;
+        }
+        return NULL ;
+
+    }
+};
