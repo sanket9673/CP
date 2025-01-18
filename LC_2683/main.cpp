d3ebc1ab567dc7fe9e36491dd0dc6b09c7e2c3b3
sanket9673 <sanketch9673@gmail.com>
2025-01-18 19:50:43 +0530

    Neighbouring bitwise XOR

---
+++ LC_2683/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        // CIRCULAR DEPENDENCY 
+        int n = derived.size() ;
+        vector<int> res ;
+        res.push_back(0) ; // LETS ASSUME 
+
+        for (int i = 1 ; i < n; i++) {
+            res.push_back(res[i-1] ^ derived[i-1]) ;
+        }
+
+        if (res[0] == (derived[n-1] ^ res[n-1])) {   // IF original[0]=original[1]⊕derived[1] 
+            return true ;  // THEN ORIGINAL ARRAY EXISTS
+        }
+        else {
+            return false ;
+        }
+    }
+};
