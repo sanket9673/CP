c04c780fd314304d88bd88dfa366014d2492cfff
sanket9673 <sanketch9673@gmail.com>
2025-02-09 15:01:39 +0530

    Check if one string swap can make string equal

---
+++ LC_1790/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if (s1 == s2) return true ;
+
+        vector<int> arr(256, 0) ;
+        int count = 0 ;
+        for (int i = 0; i < s1.size(); i++) {
+            arr[s1[i]]++ ;
+            arr[s2[i]]-- ;
+            if (s1[i] != s2[i]) count++ ;
+            if (count > 2) return false ;
+        }
+        for (int x : arr) {
+            if (x != 0) {
+                return false ;
+            }
+        }
+        return true ;
+    }
+};
