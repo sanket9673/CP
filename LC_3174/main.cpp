05fc6bea80026e828e5c8fd6d5bf92cc88d16e76
sanket9673 <sanketch9673@gmail.com>
2025-02-10 23:19:24 +0530

    Clear Digit

---
+++ LC_3174/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        string clearDigits(string s) {
+            stack<char> x ;
+            string res = "" ;
+            for (int i = 0 ; i < s.length(); i++) {
+                if (!isdigit(s[i]))   x.push(s[i]) ;
+                else {
+                    if (x.empty()) continue ;
+                    else x.pop() ;
+                }
+            }
+            while (!x.empty()) {
+                res = x.top() + res;
+                x.pop() ;
+            }
+            return res ;
+        }
+    };
