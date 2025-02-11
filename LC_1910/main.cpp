f9b5983b141a2b60353d322991e15380fed69ae4
sanket9673 <sanketch9673@gmail.com>
2025-02-11 23:41:02 +0530

    Remove all occurence of substring

--- LC_1910/main.cpp
+++ LC_1910/main.cpp
@@ -14,33 +14,35 @@ class Solution {
             //     q++ ;
             // }
             // return s ;
-            int p = part.length() ;
-            stack<char> x ;
-            if (s.length() < part.length()) return s ;
-    
-            for (int i = 0 ; i < s.length(); i++) {
-                x.push(s[i]) ;
-                if (x.top() == part[p - 1]) {
-                    string match = "" ;
-                    bool found = false ;
-                    while (!x.empty()) {
-                        match = x.top() + match ;
-                        x.pop() ;
-                        if (match == part) {
-                            found = true ;
-                            break ;
-                        } 
-                    }
-                    if (found == false) {
-                        for (int k = 0 ; k < match.size(); k++) x.push(match[k]) ;
-                    }
+            
+        // THIS CAN ALSO BE SOLVED USING STRING substr ....eg) res.substr(position) == part then removing it
+        int p = part.length() ;
+        stack<char> x ;
+        if (s.length() < part.length()) return s ;
+
+        for (int i = 0 ; i < s.length(); i++) {
+            x.push(s[i]) ;
+            if (x.top() == part[p - 1]) {
+                string match = "" ;
+                bool found = false ;
+                while (!x.empty()) {
+                    match = x.top() + match ;
+                    x.pop() ;
+                    if (match == part) {
+                        found = true ;
+                        break ;
+                    } 
+                }
+                if (found == false) {
+                    for (int k = 0 ; k < match.size(); k++) x.push(match[k]) ;
                 }
             }
-            string res = "" ;
-            while (!x.empty()) {
-                res = x.top() + res ;
-                x.pop() ;   
-            }
-            return res ;
         }
-    };
+        string res = "" ;
+        while (!x.empty()) {
+            res = x.top() + res ;
+            x.pop() ;   
+        }
+        return res ;
+    }
+};
