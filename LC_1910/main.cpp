27e747ae9d233046c8bb700b450422a347e0ac20
sanket9673 <sanketch9673@gmail.com>
2025-02-11 23:36:55 +0530

    Remove all occurence of substring

---
+++ LC_1910/main.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    public:
+        string removeOccurrences(string s, string part) {
+            // int p = 0, q = part.length() ;
+            // while (q < s.length()) {
+            //     string match = "" ;
+            //     for (int i = p; i < q; i++) match += s[i] ;
+            //     if (match == part) s.erase(p, q+1) ;
+            //     p++ ;
+            //     q++ ;
+            // }
+            // return s ;
+            int p = part.length() ;
+            stack<char> x ;
+            if (s.length() < part.length()) return s ;
+    
+            for (int i = 0 ; i < s.length(); i++) {
+                x.push(s[i]) ;
+                if (x.top() == part[p - 1]) {
+                    string match = "" ;
+                    bool found = false ;
+                    while (!x.empty()) {
+                        match = x.top() + match ;
+                        x.pop() ;
+                        if (match == part) {
+                            found = true ;
+                            break ;
+                        } 
+                    }
+                    if (found == false) {
+                        for (int k = 0 ; k < match.size(); k++) x.push(match[k]) ;
+                    }
+                }
+            }
+            string res = "" ;
+            while (!x.empty()) {
+                res = x.top() + res ;
+                x.pop() ;   
+            }
+            return res ;
+        }
+    };
