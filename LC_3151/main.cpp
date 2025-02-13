96a4f26525914203b67384a74c44a35c41db696b
sanket9673 <sanketch9673@gmail.com>
2025-02-14 02:13:57 +0530

    Special Array I

---
+++ LC_3151/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        int checkparity(int n) {
+            int count = 0 ;
+            while (n != 0) {
+                if (n & 1 == 1) count++ ;
+                n = n << 1 ;
+            }
+            return count ;
+        }
+        bool isArraySpecial(vector<int>& nums) {
+            vector<int> par ;
+            for (int x : nums) par.push_back(checkparity(x)) ;
+            for (int i = 0 ; i < par.size() - 1; i++) {
+                if (par[i] == par[i+1]) return false ;
+            }
+            return true ;
+        }
+    };
