7a1980147d81275af1bb9508215c5a376af16321
sanket9673 <sanketch9673@gmail.com>
2025-02-19 00:59:16 +0530

    Minimum Operation to make binary element array element equal to one I

---
+++ LC_3191/main.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            int res = 0;
+            bool possible = true ;
+            for (int i = 0; i < nums.size() - 2; i++) {
+                if (nums[i] == 0) {
+                    nums[i] = 1 - nums[i] ;
+                    nums[i+1] = 1 - nums[i+1] ;
+                    nums[i+2] = 1 - nums[i+2] ;
+                    res++ ;
+                } 
+            }
+            for (int i : nums) {
+                if (i == 0) {
+                    possible = false ;
+                    break ;
+                }
+            }
+            if (possible) {
+                return res ;
+            }
+            return -1 ;
+        }
+    };
