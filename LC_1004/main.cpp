53bac294b81b9f528901cf78981272be5ff9df7f
sanket9673 <sanketch9673@gmail.com>
2025-02-20 23:09:05 +0530

    Max consecutive ones III

---
+++ LC_1004/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int longestOnes(vector<int>& nums, int k) {
+            int p = 0 , q = 0, count0 = 0, res = INT_MIN ;
+            while (q < nums.size()) {
+                if (nums[q] == 0) count0++ ;
+                while (count0 > k) {
+                    if (nums[p] == 0) {
+                        count0-- ;    
+                    }
+                    p++ ;
+                }
+                res = max(res, (q-p+1)) ;
+                q++ ;
+            }
+            return res ;
+        }
+    };
