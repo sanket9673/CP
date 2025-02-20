f9aa58b80b54cdace4ab5dc745cdc27ab4383485
sanket9673 <sanketch9673@gmail.com>
2025-02-20 23:25:10 +0530

    Longest Subarray of 1's after deleting one element

---
+++ LC_1493/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int longestSubarray(vector<int>& nums) {
+            int k = 1, p = 0, q = 0, res = INT_MIN, count0 = 0 ;
+            while (q < nums.size()) {
+                if (nums[q] == 0) count0++ ;
+                while (count0 > k) {
+                    if (nums[p] == 0) {
+                        count0-- ;
+                    }
+                    p++ ;
+                }
+                res = max(res, q-p) ;
+                q++ ;
+            }
+            return res ;
+        }
+    };
