6e62af2e99e8f161888d9585cde945f34a21150b
sanket9673 <sanketch9673@gmail.com>
2025-02-21 15:39:28 +0530

    Longest Nice Subarray

---
+++ LC_2401/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int longestNiceSubarray(vector<int>& nums) {
+            int p = 0, res = 1, mask = nums[0] ;
+            for (int i = 1; i < nums.size(); i++) {
+                while ((mask & nums[i]) != 0) {
+                    mask ^= nums[p] ;
+                    p++ ;
+                }
+                mask |= nums[i] ;
+                res = max(res, i - p + 1) ;
+            }
+            return res ;
+        }
+    };
