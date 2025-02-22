199cd787c7948c4a45014a47c42c68ad4df9a1b0
sanket9673 <sanketch9673@gmail.com>
2025-02-23 02:15:07 +0530

    Minimum Operation to make array empty

---
+++ LC_2870/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int minOperations(vector<int>& nums) {
+            unordered_map<int, int> x ;
+            for (int i : nums) x[i]++ ;
+            int res = 0 ;
+            bool poss = true ;
+            for (auto val : x) {
+                if (val.second == 1) return -1 ;
+                if (val.second % 3 == 0) res += val.second / 3 ;
+                // 3 3 --- 3 2 2 (3 borrowed and converted to 2 2 group)
+                else res += (val.second / 3) + 1 ;
+            }
+            return res;
+        }
+    };
