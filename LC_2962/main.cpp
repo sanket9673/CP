269d52e1ea486df8c365d64ed0517c550505cfea
sanket9673 <sanketch9673@gmail.com>
2025-02-25 19:47:46 +0530

    count subarrays where max element appears atleast k times

---
+++ LC_2962/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        long long countSubarrays(vector<int>& nums, int k) {
+            int p = 0, q = 0 ; 
+            long long count = 0 , res = 0;
+            unordered_map<int, int> x ;
+            int mx = *max_element(nums.begin(), nums.end()) ;
+            while (q < nums.size()) {
+                if (nums[q] == mx) count++ ;
+    
+                while (count >= k) {
+                    if (nums[p] == mx) count-- ;
+                    p++ ;
+                    res += nums.size() - q ;
+                }
+                q++ ;
+            }
+            return res ;
+        }
+    };
