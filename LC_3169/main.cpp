b0db255c12b2da3fb7e4a2134009ea082eb031b4
sanket9673 <sanketch9673@gmail.com>
2025-02-26 18:29:13 +0530

    count days without meetings

---
+++ LC_3169/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int countDays(int days, vector<vector<int>>& meetings) {
+            sort(meetings.begin(), meetings.end()) ;
+        
+            int a = meetings[0][0], b = meetings[0][1] ;
+            for (int i = 1; i < meetings.size(); i++) {
+                if (meetings[i][0] <= b) {
+                    b = max(b, meetings[i][1]) ;
+                }
+                else {
+                    days -= (b - a + 1) ;
+                    a = meetings[i][0] ;
+                    b = meetings[i][1] ;
+                }
+            }
+            days -= (b - a + 1) ;
+            return days ;
+        }
+    };
