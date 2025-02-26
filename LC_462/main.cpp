05ac0748159f848f97880b517b00bc4d5ae4ea99
sanket9673 <sanketch9673@gmail.com>
2025-02-27 04:08:13 +0530

    Most Common Word

---
+++ LC_462/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int minMoves2(vector<int>& nums) {
+            sort(nums.begin(), nums.end()) ;
+            int res = 0, x = 0 ;
+            if (nums.size() % 2 != 0) {
+                x = (nums[nums.size() / 2] + nums[(nums.size() - 1) / 2]) / 2 ;
+            }
+            else x = nums[nums.size() / 2];
+    
+            for (int i : nums) {
+                res += abs(x - i) ;
+            }
+            return res ;
+        }
+    };
