5d1abcddbc4604594363c754c1bcd318f8d16b56
sanket9673 <sanketch9673@gmail.com>
2025-02-27 03:41:11 +0530

    Most Common Word

---
+++ LC_819/main.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    public:
+        string mostCommonWord(string paragraph, vector<string>& banned) {
+            unordered_set<string> x ;
+            unordered_map<string, int> y ;
+            for (string s : banned) x.insert(s) ;
+    
+            string s = "" ;
+            for (int i = 0 ; i < paragraph.length(); i++) {
+                if (isalpha(paragraph[i])) {
+                    s += tolower(paragraph[i]) ;
+                }
+                else {
+                    if (x.count(s) == 0 && !s.empty()) {
+                        y[s]++ ;
+                    }
+                    s = "" ;
+                }
+            }
+            
+            if (!s.empty()) {
+                if (x.count(s) == 0) {
+                        y[s]++ ;
+                    }
+            }
+    
+            string res = "" ;
+            int mx = INT_MIN ;
+            for (auto &val : y) {
+                if (mx < val.second) {
+                    mx = val.second ;
+                    res = val.first ;
+                }
+            }
+            return res ;
+        }
+    };
