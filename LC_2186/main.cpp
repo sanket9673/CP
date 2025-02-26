3ab195353cc368a08081ad507ae46ca0cc12b374
sanket9673 <sanketch9673@gmail.com>
2025-02-27 05:11:04 +0530

    Minimum number of steps to make two strings anagram II

---
+++ LC_2186/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int minSteps(string s, string t) {
+            unordered_map<char , int> x ;
+            for (char c : s) x[c]++ ;
+            // for (char c : t) {
+            //     if (x.count(c) == 0) x[c]++ ;
+            //     else x[c]-- ;
+            // }                            DOESNT WORK
+            for (char c : t) x[c]--;
+            int res  = 0 ;
+            for (auto &val : x) res += abs(val.second) ;
+            return res ;
+        }
+    };
