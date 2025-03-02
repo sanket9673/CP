58cc78775907983c668bee6b43ba75137a37c033
sanket9673 <sanketch9673@gmail.com>
2025-03-02 18:55:09 +0530

    Minimum Moves to reach target score

---
+++ LC_2139/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int minMoves(int target, int maxDoubles) {
+            int start = 1, mx = maxDoubles, res = 0;
+            if (maxDoubles == 0) {
+                return target - start ;
+            }
+            while (target != 1) {
+                if (maxDoubles > 0 && target % 2 == 0) {
+                    maxDoubles-- ;
+                    target /= 2 ;
+                }
+                else {
+                    target -= 1 ;
+                }
+                res++ ;
+            }
+            return res ;
+        }
+    };
