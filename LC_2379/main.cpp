cffa774f846b0a29b131747cfc808193e98e9f84
sanket9673 <sanketch9673@gmail.com>
2025-03-08 10:01:16 +0530

    Minimum recolors to get K consecutive black boxes

---
+++ LC_2379/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            // SLIDING WINDOW
+            int res = INT_MAX ;
+            for (int i = 0 ; i < blocks.size() - k + 1; i++) {
+                int countw = 0 ;
+                for (int j = 0 ; j < k; j++) {
+                    if (blocks[i + j] == 'W') countw++ ;
+                }
+                res = min(res, countw) ;
+            }
+            return res ;
+            
+        }
+    };
